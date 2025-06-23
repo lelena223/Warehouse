afd0af5e6c122ec22746e044c4606eedb2b75a0a
lelena <lelenahkht2003@gmail.com>
2025-06-23 11:21:31 +0700

    Update check input data

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError.util";
 
 export const errorHandler = (
   err: any,
@@ -6,7 +7,11 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error("Error:", err);
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
 
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error",
--- src/services/receipts.service.ts
+++ src/services/receipts.service.ts
@@ -1,6 +1,38 @@
 import prisma from "../prismaClient";
+import { AppError } from "../utils/appError.util";
 
 export const createReceipt = async (dataBody: any) => {
+  if (!dataBody.items) {
+    throw new AppError("Missing items", 400);
+  }
+  if (!dataBody.warehouseId) {
+    throw new AppError("Missing warehouseId", 400);
+  }
+  if (!dataBody.supplierId) {
+    throw new AppError("Missing supplierId", 400);
+  }
+
+  const warehouse = await prisma.warehouse.findUnique({
+    where: { id: dataBody.warehouseId },
+  });
+  if (!warehouse) {
+    throw new AppError("Warehouse not found", 400);
+  }
+  const supplier = await prisma.supplier.findUnique({
+    where: { id: dataBody.supplierId },
+  });
+  if (!supplier) {
+    throw new AppError("Supplier not found", 400);
+  }
+
+  const existed = await prisma.importReceipt.findUnique({
+    where: { code: dataBody.code },
+  });
+
+  if (existed) {
+    throw new AppError("Code already exists", 400);
+  }
+
   const receipt = await prisma.importReceipt.create({
     data: {
       ...dataBody,
